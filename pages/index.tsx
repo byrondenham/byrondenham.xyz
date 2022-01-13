2adcd92d1ea011c95217bb90cd28d4ceb0b3aa56
byrondenham <84453909+byrondenham@users.noreply.github.com>
2022-01-13 13:52:31 +0000

    background button

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,20 @@ import { useState } from "react";
 
 const Home: NextPage = () => {
   const [name, setName] = useState("person");
+  const [bgColor, setBgColor] = useState("ffffff")
 
   return (
     <>
       <Head>
         <title>Byron Denham</title>
+        <style>
+          background: #{bgColor}
+        </style>
       </Head>
       <h1>Hello, {name}.</h1>
       <h2>What is your name?</h2>
       <input type="text" onChange={e => setName(e.target.value)} />
+      <button onClick={() => setBgColor(Math.floor(Math.random() * 16).toString(16).join(""))}>Change colour</button>
     </>
   );
 };
